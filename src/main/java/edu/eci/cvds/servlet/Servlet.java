068b2aacb9110809ec5d19e3396c86d2b92ed059
Sebastianhh99 <39110763+Sebastianhh99@users.noreply.github.com>
2019-09-17 18:48:03 -0500

    doPost sin cambio doGet

--- src/main/java/edu/eci/cvds/servlet/Servlet.java
+++ src/main/java/edu/eci/cvds/servlet/Servlet.java
@@ -3,6 +3,7 @@ package edu.eci.cvds.servlet;
 import edu.eci.cvds.servlet.model.Todo;
 import java.io.IOException;
 import java.io.Writer;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Enumeration;
@@ -47,29 +48,32 @@ public class Servlet extends HttpServlet {
     
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    	 Writer responseWriter = resp.getWriter();
-         Optional<String> optId = Optional.ofNullable(req.getParameter("id"));
-         String id = optId.isPresent() && !optId.get().isEmpty() ? optId.get() : "-1";
-         int validId=-1;
-         int status = 0;
-         try{
-             validId = Integer.parseInt(id);
-         }catch(NumberFormatException e){
-             status = HttpServletResponse.SC_BAD_REQUEST;
-         }
-         if (status==0) status = (validId < 1 || validId > 200) ? HttpServletResponse.SC_NOT_FOUND :HttpServletResponse.SC_OK;
-         status = (req.getParameter("id") == null)? HttpServletResponse.SC_BAD_REQUEST:status;
-         status = (!optId.isPresent() && status!=HttpServletResponse.SC_BAD_REQUEST)? HttpServletResponse.SC_INTERNAL_SERVER_ERROR:status;
-         resp.setStatus(status);
-         if(status == 200){
-             ArrayList<Todo> todoList = new ArrayList();
-             todoList.add(Service.getTodo(validId));
-             String htmlTable=Service.todosToHTMLTable(todoList);
-             responseWriter.write(htmlTable);
-         }else{
-             responseWriter.write("Error "+status);
-         }
-         responseWriter.flush();
+    	int status =HttpServletResponse.SC_OK;
+    	Writer responseWriter=null;
+    	try {
+    		responseWriter = resp.getWriter();
+            Optional<String> optId = Optional.ofNullable(req.getParameter("id"));
+            String id = optId.isPresent() && !optId.get().isEmpty() ? optId.get() : "-1";
+            int validId=-1;
+            try {
+            	validId=Integer.parseInt(id);
+            }catch(NumberFormatException e) {
+            	status = HttpServletResponse.SC_BAD_REQUEST;
+            }
+            if (status==0) status = (validId < 1 || validId > 200) ? HttpServletResponse.SC_NOT_FOUND :HttpServletResponse.SC_OK;
+            status = (req.getParameter("id") == null)? HttpServletResponse.SC_BAD_REQUEST:status;
+            resp.setStatus(status);
+    	}catch(MalformedURLException e) {
+    		status=HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+    	}catch(Exception e) {
+    		status=HttpServletResponse.SC_BAD_REQUEST;
+    	}
+    	if(status==200) {
+    		
+    	}else {
+    		responseWriter.write("Error "+status);
+    	}
+    	responseWriter.flush();
     }
 }
                   
