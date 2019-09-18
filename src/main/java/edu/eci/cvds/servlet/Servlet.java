37a0712047b6743f8ce701e9c51443f587435164
Sebastianhh99 <39110763+Sebastianhh99@users.noreply.github.com>
2019-09-17 19:22:07 -0500

    Cuadro y submit funcionando

--- src/main/java/edu/eci/cvds/servlet/Servlet.java
+++ src/main/java/edu/eci/cvds/servlet/Servlet.java
@@ -21,29 +21,35 @@ import javax.servlet.http.HttpServletResponse;
 public class Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Writer responseWriter = resp.getWriter();
-        Optional<String> optId = Optional.ofNullable(req.getParameter("id"));
-        String id = optId.isPresent() && !optId.get().isEmpty() ? optId.get() : "-1";
-        int validId=-1;
-        int status = 0;
-        try{
-            validId = Integer.parseInt(id);
-        }catch(NumberFormatException e){
-            status = HttpServletResponse.SC_BAD_REQUEST;
-        }
-        if (status==0) status = (validId < 1 || validId > 200) ? HttpServletResponse.SC_NOT_FOUND :HttpServletResponse.SC_OK;
-        status = (req.getParameter("id") == null)? HttpServletResponse.SC_BAD_REQUEST:status;
-        status = (!optId.isPresent() && status!=HttpServletResponse.SC_BAD_REQUEST)? HttpServletResponse.SC_INTERNAL_SERVER_ERROR:status;
-        resp.setStatus(status);
-        if(status == 200){
-            ArrayList<Todo> todoList = new ArrayList();
+    	int status =HttpServletResponse.SC_OK;
+    	Writer responseWriter=null;
+    	int validId=-1;
+    	try {
+    		responseWriter = resp.getWriter();
+            Optional<String> optId = Optional.ofNullable(req.getParameter("id"));
+            String id = optId.isPresent() && !optId.get().isEmpty() ? optId.get() : "-1";
+            try {
+            	validId=Integer.parseInt(id);
+            }catch(NumberFormatException e) {
+            	status = HttpServletResponse.SC_BAD_REQUEST;
+            }
+            if (status==200) status = (validId < 1 || validId > 200) ? HttpServletResponse.SC_NOT_FOUND :HttpServletResponse.SC_OK;
+            status = (req.getParameter("id") == null)? HttpServletResponse.SC_BAD_REQUEST:status;
+            resp.setStatus(status);
+    	}catch(MalformedURLException e) {
+    		status=HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+    	}catch(Exception e) {
+    		status=HttpServletResponse.SC_BAD_REQUEST;
+    	}
+    	if(status==200) {
+    		ArrayList<Todo> todoList = new ArrayList();
             todoList.add(Service.getTodo(validId));
             String htmlTable=Service.todosToHTMLTable(todoList);
             responseWriter.write(htmlTable);
-        }else{
-            responseWriter.write("Error "+status);
-        }
-        responseWriter.flush();
+    	}else {
+    		responseWriter.write("Error "+status);
+    	}
+    	responseWriter.flush();
     }
     
     @Override
@@ -60,7 +66,7 @@ public class Servlet extends HttpServlet {
             }catch(NumberFormatException e) {
             	status = HttpServletResponse.SC_BAD_REQUEST;
             }
-            if (status==0) status = (validId < 1 || validId > 200) ? HttpServletResponse.SC_NOT_FOUND :HttpServletResponse.SC_OK;
+            if (status==200) status = (validId < 1 || validId > 200) ? HttpServletResponse.SC_NOT_FOUND :HttpServletResponse.SC_OK;
             status = (req.getParameter("id") == null)? HttpServletResponse.SC_BAD_REQUEST:status;
             resp.setStatus(status);
     	}catch(MalformedURLException e) {
